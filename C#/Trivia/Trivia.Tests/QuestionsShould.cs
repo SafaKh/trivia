a42b5683813caa38cd1d07ba48784bb3fa5e5907
p0904294 <skhelifa1@gmail.com>
2017-05-11 15:29:45 +0200

    utilisation de 5 categorie

---
+++ C#/Trivia/Trivia.Tests/QuestionsShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Trivia.Tests
+{
+    class QuestionsShould
+    {
+        [Test]
+        public void AllowToPlayWith5Category()
+        {
+            var stringWriter = new StringWriter();
+            var previousConsoleOut = Console.Out;
+            Console.SetOut(stringWriter);
+            var questions=new Questions(); 
+            questions.AskQuestion(4);
+            Console.SetOut(previousConsoleOut);
+            Assert.That(stringWriter.ToString().Contains("Cinema"));
+        }
+    }
+}
