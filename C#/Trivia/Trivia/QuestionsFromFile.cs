93fb32914ecc725203ede252a50e6d85495585db
p0904294 <skhelifa1@gmail.com>
2017-06-28 08:53:47 +0200

    Ajout interface QuestionsFromFile et implementation

--- C#/Trivia/Trivia/QuestionsFromFile.cs
+++ C#/Trivia/Trivia/QuestionsFromFile.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Trivia
 {
-    class QuestionsFromFile
+    class QuestionsFromFile : IQuestionsRepository
     {
         public LinkedList<string> GetQuestions(string category)
-        { 
-            return null;
-            
+        {
+            var questions = new LinkedList<String>();
+            using (var filestream = File.OpenRead(""))
+            //StreamReader permet de lire ligne par ligne le fichier .txt
+            using (var streamReader = new StreamReader(filestream))
+            {
+                while (streamReader.EndOfStream)
+                {
+                    questions.AddLast(streamReader.ReadLine());
+                }
+            }
+            return questions; 
+
+
         }
 
     }
